30b7b87c095579e24e8b54fc1a049aa3ef207072
SomlaI Gábor <somlaigabor75@gmail.com>
2020-08-28 21:14:20 +0200

    Added endpoint for created user

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,16 @@
-# from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from .serializers import UserSerializer, AuthTokenSerialSerializer
 # Create your views here.
+
+
+class CreateUserView(generics.CreateAPIView):
+    """ Leírás: Create a new user in the system """
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """ Leírás: Create a new auth token for user """
+    serializer_class = AuthTokenSerialSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
