939b580e4d9e245dbe63d90b4b19f46b70a5123f
SomlaI Gábor <somlaigabor75@gmail.com>
2020-08-31 19:52:32 +0200

    Added manage user endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**param):
@@ -17,7 +18,7 @@ def create_user(**param):
 class PublicUserApiTests(TestCase):
     """ Leírás: Test the users API public """
 
-    def setUo(self, ):
+    def setUp(self, ):
         """ Leírás:  """
         self.client = APIClient()
 
@@ -95,3 +96,51 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self, ):
+        """ Leírás: Test that authentication is required for users """
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTest(TestCase):
+    """ Leírás: Test API requests that require authentication """
+
+    def setUp(self) -> None:
+        """ Leírás:  """
+        self.user = create_user(
+            email='test@londonappdev.com',
+            password='testpass',
+            name='name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self, ):
+        """ Leírás: Test retrieving profile for logged user """
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self, ):
+        """ Leírás: Test that POST is not allowed on the me url """
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self, ):
+        """ Leírás: Test updating the user profile for authenticated user """
+        payload = {'name': 'newname', 'password': 'newpassword123'}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        # userr = User.objects.get(name=payload['name'])
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
