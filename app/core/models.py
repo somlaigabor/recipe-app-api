b32e59cc2005411708dc27f32b96600cf348f806
SomlaI Gábor <somlaigabor75@gmail.com>
2020-09-03 22:14:13 +0200

    Added recipe modeé

--- app/core/models.py
+++ app/core/models.py
@@ -64,3 +64,21 @@ class Ingredient(models.Model):
     def __str__(self):
         """ Leírás:  """
         return self.name
+
+
+class Recipe(models.Model):
+    """ Leírás: Recipe objects """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        """ Leírás:  """
+        return self.title
